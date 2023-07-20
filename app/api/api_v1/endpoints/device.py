d3694173a02e0a03ac74bc9499a5984ea9985cb0
zerg0088 <jeonsangsu@jeonsangsuui-MacBookPro.local>
2023-07-20 17:40:22 +0900

    sms change

--- app/api/api_v1/endpoints/device.py
+++ app/api/api_v1/endpoints/device.py
@@ -1,5 +1,5 @@
 import os
-from twilio.rest import Client
+#from twilio.rest import Client
 
 from typing import Any
 
@@ -16,12 +16,16 @@ from app.crud.user import crud_user
 from app.crud.place import crud_place
 from app.crud.device_value import crud_device_value
 from fastapi.encoders import jsonable_encoder
-from app.core.config import settings
+#from app.core.config import settings
 import csv
 from io import StringIO
 from fastapi.responses import StreamingResponse
+import base64
+import requests
+import re
 
-client = Client(settings.SMS_SID, settings.SMS_AUTH_TOKEN)
+
+#client = Client(settings.SMS_SID, settings.SMS_AUTH_TOKEN)
 router = APIRouter()
 
 @router.get("/device_list", response_model=list[DeviceRead])
@@ -143,7 +147,6 @@ def insert_data(did: int, v1 : int = None, v2 : int = None, v3 : int = None,
             for phone in user.phone :
                 if len(phone) > 5 :
                     phoneSet.add(phone)
-        
     if(e1 > 0) : 
         if(device.latest_ch1_error == 0) :
             device.latest_ch1_error = 1
@@ -190,10 +193,68 @@ def insert_data(did: int, v1 : int = None, v2 : int = None, v3 : int = None,
 
 def sendSms(position, name, ch, list) :
     for number in list : 
-        temp = "+82" + number[1:]  
-        print(temp)
-        message = client.messages.create(
-            body= position + ' ' + name + '고전압기 CH.' + str(ch) + '에 문제가 발생하였습니다.',
-            from_='+12705618731',
-            to=temp
-        )    
+        message = position + ' ' + name + '고전압기 CH.' + str(ch) + '에 문제가 발생하였습니다.'
+        send_message(message, format_phone_number(number))
+        # message = client.messages.create(
+        #     body= position + ' ' + name + '고전압기 CH.' + str(ch) + '에 문제가 발생하였습니다.',
+        #     from_='+12705618731',
+        #     to=temp
+        # )    
+        
+def send_message(msg, rphone):
+    sms_url = 'https://sslsms.cafe24.com/sms_sender.php'
+
+    user_id = base64.b64encode("alink2016".encode('utf-8'))
+    secure = base64.b64encode("7fa0085fa5cb29565a1d27fe0d93c594".encode('utf-8')) 
+    mode = base64.b64encode('1'.encode('utf-8'))
+    SMS_SENDER_NUMBER = ["051", "972", "3693"]
+    
+    sphone1 = base64.b64encode(SMS_SENDER_NUMBER[0].encode('utf-8'))
+    sphone2 = base64.b64encode(SMS_SENDER_NUMBER[1].encode('utf-8'))
+    sphone3 = base64.b64encode(SMS_SENDER_NUMBER[2].encode('utf-8'))
+
+    rphone = base64.b64encode(rphone.encode('utf-8'))
+    msg = base64.b64encode(msg.encode('utf-8'))
+    testflag = base64.b64encode('N'.encode('utf-8'))
+    data = {
+      'user_id': user_id,
+      'secure': secure,
+      'mode': mode,
+      'sphone1': sphone1,
+      'sphone2': sphone2,
+      'sphone3': sphone3,
+      'rphone': rphone,
+      'msg': msg,
+      'testflag' : testflag
+    }
+    decoded_data = {
+      'user_id' : base64.b64decode(user_id).decode('utf-8'),
+      'secure': base64.b64decode(secure).decode('utf-8'),
+      'mode': base64.b64decode(mode).decode('utf-8'),
+      'sphone1': base64.b64decode(sphone1).decode('utf-8'),
+      'sphone2': base64.b64decode(sphone2).decode('utf-8'),
+      'sphone3': base64.b64decode(sphone3).decode('utf-8'),
+      'rphone': base64.b64decode(rphone).decode('utf-8'),
+      'msg': base64.b64decode(msg).decode('utf-8'),
+      'testflag' : base64.b64decode(testflag).decode('utf-8')
+    }
+    print(decoded_data) 
+
+    res = requests.post(sms_url, data)
+    print(res)
+    return res.status_code
+
+def format_phone_number(text):
+    # 숫자만 추출하여 하이픈(-)을 제외한 숫자들을 합침
+    numbers = "".join(re.findall(r'\d', text))
+
+    # 핸드폰 번호 형식에 맞게 변환
+    if len(numbers) == 10:
+        formatted_number = re.sub(r'(\d{3})(\d{3})(\d{4})', r'\1-\2-\3', numbers)
+    elif len(numbers) == 11:
+        formatted_number = re.sub(r'(\d{3})(\d{4})(\d{4})', r'\1-\2-\3', numbers)
+    else:
+        # 유효한 핸드폰 번호가 아닐 경우, 원본 텍스트 반환
+        formatted_number = text
+
+    return formatted_number
